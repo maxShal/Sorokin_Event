38f6f6d5a8a38f4cb0cccb569577035de15cc971
max <max_shalash@mail.ru>
2025-05-28 10:02:01 +0600

    commit

---
+++ src/main/java/com/example/Sorokin_Event/SorokinEventApplication.java
@@ -0,0 +1,13 @@
+package com.example.Sorokin_Event;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SorokinEventApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SorokinEventApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Sorokin_Event/SorokinEventApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Sorokin_Event;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SorokinEventApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
