5dfda8e14c9b1ec473c8732f8889d0ce905e0784
max <max_shalash@mail.ru>
2025-06-17 23:55:53 +0600

    ApplicationReadyEvent update

--- src/main/java/com/example/Sorokin_Event/model/DefaultUserInitializer.java
+++ src/main/java/com/example/Sorokin_Event/model/DefaultUserInitializer.java
@@ -2,7 +2,7 @@ package com.example.Sorokin_Event.model;
 
 import com.example.Sorokin_Event.service.UserService;
 import jakarta.annotation.PostConstruct;
-import org.springframework.context.event.ContextRefreshedEvent;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.context.event.EventListener;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Component;
@@ -16,7 +16,7 @@ public class DefaultUserInitializer {
         this.userService = userService;
         this.passwordEncoder = passwordEncoder;
     }
-    @EventListener(ContextRefreshedEvent.class)
+    @EventListener(ApplicationReadyEvent.class)
     public void initUsers() {
         createUserIfNotExists("admin", "admin", Role.ADMIN);
         createUserIfNotExists("user", "user", Role.USER);
